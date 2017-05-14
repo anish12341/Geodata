ceb881ad169b664375cdf27adaaffd1f43e83629
anish12341 <anishpatel345@gmail.com>
2017-05-14 21:54:58 +0530

    Add files via upload

---
+++ where1.js
@@ -0,0 +1,12 @@
+myData = [
+[23.0267583,72.4769993, 'Below Dominos Pizza, Amrapali Axiom, Bopal Ambli Char Rasta, Bopal, Ahmedabad, Gujarat 380058, India'],
+[23.0545573,72.5557501, '62, Sundar Nagar, Naryanpura, Ahmedabad, Gujarat 380013, India'],
+[23.0284477,72.5590727, 'GF, Silicon Tower, B/h. Samartheshwar Mahadev, Law Garden, Ahmedabad, Gujarat 380009, India'],
+[13.0096323,77.551071, 'Hare Krishna Hill, Chord Road, Rajaji Nagar, Bengaluru, Karnataka 560010, India'],
+[28.6126735,77.2772619, 'Noida Mor, Pandav Nagar, New Delhi, Delhi 110092, India'],
+[23.0330967,72.5498147, 'L.D. Institute of Indology Campus, Nr. Gujarat University, Opp. L.D. Engineering Hostel, Navarangpura, Gujarat University Clock Tower, Ahmedabad, Gujarat 380009, India'],
+[23.0203135,72.5561606, 'Parimal Garden, Parimal Cross Road, Ambawadi, Ahmedabad, Gujarat 380006, India'],
+[23.1666709,72.5803486, 'Adalaj, Gujarat 382421, India'],
+[47.6620637,-117.4191283, 'Riverfront Park, 507 N Howard St, Spokane, WA 99201, USA'],
+[23.0278374,72.6009909, 'Nr. Kalupur Railway Station Road, Sakar Bazzar, Kalupur, Ahmedabad, Gujarat 380002, India']
+];
